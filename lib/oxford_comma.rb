ebc2dcf2d42858b560fcd2a813c275bf8126f97c
Brendan McIlhenny <bmcilhen@gmail.com>
2017-09-25 19:55:46 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,8 @@
 def oxford_comma(array)
   before_last_element = array.length - 1
   array[before_last_element] = "and " + array[array.length - 1]
+  if array.length == 2
+    array.join(" ")
   array.join(", ")
 end
 
