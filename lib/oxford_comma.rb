737b76f8ff130aa6e24bcddfe55f35859e804c51
Brendan McIlhenny <bmcilhen@gmail.com>
2017-09-25 19:54:15 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,7 @@
 def oxford_comma(array)
   before_last_element = array.length - 1
-  array[array.length - 1] = "and" + array[array.length - 1
-  array.insert(before_last_element, "and")
+  array[before_last_element] = "and " + array[array.length - 1]
   array.join(", ")
 end
+
+oxford_comma(['Bernard', 'Brendan', 'Billy'])
