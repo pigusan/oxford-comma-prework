bd5528b634f3a37577ca89b8e9c83c296a31a6b7
Brendan McIlhenny <bmcilhen@gmail.com>
2017-09-25 19:52:34 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,6 @@
 def oxford_comma(array)
   before_last_element = array.length - 1
-  array[array.length - 1]
+  array[array.length - 1] = "and" + array[array.length - 1
   array.insert(before_last_element, "and")
   array.join(", ")
 end
