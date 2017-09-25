4a2157c1952680a92ee832a56dcc35bde9a01675
Brendan McIlhenny <bmcilhen@gmail.com>
2017-09-25 20:05:04 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array[0]
+  elsif array.length == 2
+    before_last_element = array.length - 1
+    array[before_last_element] = "and " + array[array.length - 1]
+    array.join(" ")
+  else
+    before_last_element = array.length - 1
+    array[before_last_element] = "and " + array[array.length - 1]
+    array.join(", ")
+  end
 end
+
+oxford_comma(['Bernard', 'Brendan', 'Billy'])
